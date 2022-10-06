a98897083ea308d7923ba2e35e23623c4ee09378
Puckis03 <cederlund.anton@gmail.com>
2022-10-06 13:40:26 +0200

    pain
    camera

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+
+    }
+
+
+
+    private void Update()
+    {
+
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 10, player.transform.position.z);
+        transform.LookAt(player.transform.position);
+    }
+
+}
